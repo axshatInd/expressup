c8a2e60db13d1b28282bbf24beb3e033c060be2e
Akshat Saini <axshat.ind@gmail.com>
2024-08-17 05:23:22 +0530

    Built Login page frontend

--- components/Login.js
+++ components/Login.js
@@ -8,12 +8,13 @@ export default function Login() {
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
       <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>Log In / Register</h3>
       <p>You're one step away!</p>
-      <input className='w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo' placeholder='Email'/>
-      <input className='w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo' placeholder='Password' type='password'/>
+      <input className='w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo rounded-full outline-none' placeholder='Email'/>
+      <input className='w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo rounded-full outline-none' placeholder='Password' type='password'/>
 
       <div className='max-w-[400px] w-full mx-auto'>
       <Button text = "Submit" full/>
       </div>
+      <p className='text-center'>Don't have an account? <span className='text-indigo-600'>Sign up!</span></p>
 
     </div>
   )
