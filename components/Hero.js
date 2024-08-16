d10f53b9db4c376dca8267c3b1828a2693942f72
Akshat Saini <axshat.ind@gmail.com>
2024-08-17 03:57:11 +0530

    Gave the tagline

--- components/Hero.js
+++ components/Hero.js
@@ -7,10 +7,10 @@ export default function Hero() {
   return (
     <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
         <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}>
-            <span className='textGradient'>ExpressUp</span> lets you express your <span className='textGradient'>Daily Mood!</span>
+            <span className='textGradient'>ExpressUp</span> lets you record your <span className='textGradient'>Daily Mood!</span>
         </h1>
-        <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>
-            Create your mood record and see how you feel <span className='font-semibold'>EVERY DAY</span>
+        <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px] font-semibold'>
+            "Every Day Tells a Story, <span className='text-indigo-600 font-semibold'>What's Yours?</span>"
         </p>
         <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
             <Button text="Login" dark /> {/* Login button with white border */}
