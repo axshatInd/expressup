7e3ad9621fc24b2551a8a41e12afe055b2729362
Akshat Saini <axshat.ind@gmail.com>
2024-08-17 22:44:23 +0530

    created calender layout

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { gradients, baseRating } from '@/utils'
 
 
 const months = { 'January': 'Jan', 'February': 'Feb', 'March': 'Mar', 'April': 'Apr', 'May': 'May', 'June': 'Jun', 'July': 'Jul', 'August': 'Aug', 'September': 'Sept', 'October': 'Oct', 'November': 'Nov', 'December': 'Dec' }
 const now = new Date()
 const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+const data = {
+  "15" : 2, "16" : 4, "17" : 1, "18" : 3, "19" : 5, "20" : 2, "21" : 4, "22" : 1, "23" : 3, "24" : 5, 
+}
 
-export default function Calendar() {
+
+export default function Calendar(props) {
+  const {demo} = props
   const year = 2024
   const month = 'August'
   const monthNow = new Date(year, Object.keys(months).indexOf(month), 1)
@@ -25,7 +31,7 @@ export default function Calendar() {
     <div className='flex flex-col overflow-hidden gap-1'> 
       {[...Array(numRows).keys()].map((row, rowIndex) => {
         return (
-          <div key = {rowIndex}>
+          <div key = {rowIndex} className='font-semibold grid grid-cols-7 gap-1'>
             {dayList.map((dayOfWeek, dayOfWeekIndex) => {
               let dayIndex = (rowIndex * 7) + dayOfWeekIndex - (firstDayOfMonth - 1) /* this will calculate where the day currently is in */
 
@@ -40,12 +46,15 @@ export default function Calendar() {
                 )
               }
 
+              let color = demo ?
+              gradients.yellow[baseRating[dayIndex]] : dayIndex in data ? gradients.yellow[data[dayIndex]] : 'white'
 
 
 
               return (
-                <div key = {dayOfWeekIndex}>
-                  randominput
+                <div className={'text-xs sm:text-sm border border-solid p-2 flex items-center gap-2 justify-between rounded-lg' + (isToday ? ' border-indigo-400' : ' border-indigo-100') + (color === 'white' ? 'text-indigo-400' : 'text-white')} key = {dayOfWeekIndex}>
+                  
+                  <p>{dayIndex}</p>
 
                 </div>
               )
