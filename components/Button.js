49982a9dbbc885814711acd46b4255592c3da5e2
Akshat Saini <axshat.ind@gmail.com>
2024-08-17 23:50:25 +0530

    Changed Login and Sign Up buttons to gradient of amber as well

--- components/Button.js
+++ components/Button.js
@@ -10,9 +10,9 @@ export default function Button(props) {
         <button className={
             'relative rounded-full overflow-hidden duration-200 hover:opacity-60 border-2 ' +
             (dark ? 
-                'text-white bg-gradient-to-r from-[#FF9A8B] via-[#FF6F91] to-[#C6A0C6] border-white shadow-md shadow-[#4a4a4a]' 
+                'text-white bg-gradient-to-r from-[#FDBB2D] via-[#FF9A00] to-[#FFD700] border-white shadow-md shadow-[#4a4a4a]' 
                 : 
-                'text-white bg-gradient-to-r from-[#FF9A8B] via-[#FF6F91] to-[#C6A0C6] border-transparent shadow-md shadow-[#4a4a4a]' + (full ? 'grid place-items-center w-full ' : ' ')
+                'text-white bg-gradient-to-r from-[#FDBB2D] via-[#FF9A00] to-[#FFD700] border-transparent shadow-md shadow-[#4a4a4a]' + (full ? 'grid place-items-center w-full ' : ' ')
             )
         }>
             <p className={'px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ' + fugaz.className}>
