45ef725367c11a074c96ddd9962ec20a612bafb8
Akshat Saini <axshat.ind@gmail.com>
2024-08-17 23:37:17 +0530

    added new gradients

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,23 @@
 export const gradients = {
-    indigo: ['#dcd6ff', '#b8adff', '#9285ff', '#7766ff', '#4833ff', '#3525db', '#261ab1', '#1a1093', '#10097a',],
-    green: ['#dcfdc3', '#affc9d', '#7cf86c', '#4bf246', '#0cea1c', '#0dc928', '#0ca82f', '#038731', '#047031',],
-    blue: ['#ccfffa', '#9afefe', '#66f1fc', '#41dffa', '#07c2f7', '#0497d4', '#0171b1', '#02518e', '#003a76',],
-    yellow: ['#fff8db', '#fff0b8', '#ffe495', '#ffd97b', '#ffc84f', '#dba339', '#b78127', '#936118', '#7a4b10',],
-    pink: ['#ffd8f2', '#ffb1ea', '#ff8aea', '#ff6df1', '#ff3dfe', '#cd2ddb', '#9d1fb7', '#731493', '#540b7a',],
+    indigo: ['#dcd6ff', '#b8adff', '#9285ff', '#7766ff', '#4833ff', '#3525db', '#261ab1', '#1a1093', '#10097a'],
+    green: ['#dcfdc3', '#affc9d', '#7cf86c', '#4bf246', '#0cea1c', '#0dc928', '#0ca82f', '#038731', '#047031'],
+    blue: ['#ccfffa', '#9afefe', '#66f1fc', '#41dffa', '#07c2f7', '#0497d4', '#0171b1', '#02518e', '#003a76'],
+    yellow: ['#fff8db', '#fff0b8', '#ffe495', '#ffd97b', '#ffc84f', '#dba339', '#b78127', '#936118', '#7a4b10'],
+    pink: ['#ffd8f2', '#ffb1ea', '#ff8aea', '#ff6df1', '#ff3dfe', '#cd2ddb', '#9d1fb7', '#731493', '#540b7a'],
+    // Additional color sets:
+    purple: ['#f3d9fa', '#e4b6f4', '#d68eef', '#c368eb', '#a946e3', '#8b2fcc', '#6d1db4', '#51139b', '#3c0b7e'],
+    red: ['#ffd6d9', '#ffb3b6', '#ff8f90', '#ff6a6d', '#ff4248', '#db2f34', '#b71f24', '#931419', '#7a0b13'],
+    orange: ['#ffe6cc', '#ffcc99', '#ffb266', '#ff9933', '#ff8000', '#cc6600', '#994c00', '#663300', '#4d2600'],
+    teal: ['#d3f7f4', '#a5eee9', '#73e5df', '#4edbd5', '#25d1ca', '#1ca6a3', '#157c7c', '#0e5757', '#093b3b'],
+    copper: ['#ffd8cc', '#ffb8a3', '#ff967d', '#ff755b', '#ff4e36', '#db3b2a', '#b72b20', '#931c17', '#7a140e'],
+    mint: ['#e0ffea', '#baffd1', '#8dffb6', '#60ff9e', '#2bff86', '#24db6c', '#1eb755', '#189342', '#137a36'],
+    // New Color Sets:
+    goldenrod: ['#fff5d9', '#ffe9b5', '#ffdb8c', '#ffcc62', '#ffb93b', '#e3a12c', '#c68b20', '#a87316', '#8f5e0e'],
+    darkOrange: ['#ffeed6', '#ffd5ac', '#ffbc83', '#ffa25d', '#ff8836', '#db6d2b', '#b75520', '#933e17', '#7a2f10'],
+    amber: ['#fff4d1', '#ffe4a3', '#ffd373', '#ffc043', '#ffaa18', '#db8613', '#b7690e', '#93500a', '#7a3e08'],
+    mustard: ['#fff2d5', '#ffe5ac', '#ffd77f', '#ffc953', '#ffba29', '#d9921f', '#b37218', '#8f5510', '#77420b'],
 }
+
 
 export const baseRating = {
     "0": 2, "1": 5, "2": 4, "3": 1, "4": 3,
